1ac0055ce7d52a33628f44c3ac138c84f1c525b4
MikeB <mikstar@live.nl>
2014-10-06 11:42:08 +0200

    scriptstart

---
+++ Assets/Scripts/BotControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BotControl : MonoBehaviour {
+
+	enum Command{none, move, attack, defend};
+
+	private Command currentCommand = Command.none;
+
+	public void giveMoveCommand()
+	{
+		if(currentCommand != Command.move)
+		{
+
+		}
+	}
+
+	public void giveAttackCommand()
+	{
+
+	}
+
+	public void giveDefendCommand()
+	{
+
+	}
+}
