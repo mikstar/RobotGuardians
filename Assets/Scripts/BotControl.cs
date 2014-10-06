7c1400632d13656e71cbc77ee207f73ca0e67f88
CoenMeijer <coennn@live.nl>
2014-10-06 12:40:41 +0200

    Movement script update

--- Assets/Scripts/BotControl.cs
+++ Assets/Scripts/BotControl.cs
@@ -6,13 +6,11 @@ public class BotControl : MonoBehaviour {
 	enum Command{none, move, attack, defend};
 
 	private Command currentCommand = Command.none;
+	private Vector3 worldPos;
 
 	public void giveMoveCommand()
 	{
-		if(currentCommand != Command.move)
-		{
 
-		}
 	}
 
 	public void giveAttackCommand()
@@ -23,5 +21,31 @@ public class BotControl : MonoBehaviour {
 	public void giveDefendCommand()
 	{
 
+	}
+
+	private void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			Ray testRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if(Physics.Raycast(testRay, out hit))
+			{
+				Debug.Log(hit);
+			}
+
+		}
+
+//		if(Input.touchCount > 0)
+//		{
+//			Ray testRay = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+//			RaycastHit hitInfo;
+//
+//			if (Physics.Raycast(testRay, out hitInfo))
+//			{
+//				
+//			}
+//		}
 	}
 }
